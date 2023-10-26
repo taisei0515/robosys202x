388b87db0213e1bcf4550b9d9fbef1107b874bb7
Taisei Fujino <raisu05150515@gmail.com>
2023-10-26 12:23:50 +0900

    a

--- test.bash
+++ test.bash
@@ -1,3 +1,21 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2022 Taisei Fujino
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng (){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+[ "${out}" =15 ] || ng ${LINENO}
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]	 || ng ${LINENO}
+[ "${out}"= "" ] || ng ${LINENO}
+
+
+[ "$res" =0 ] && echo OK
+exit $res
