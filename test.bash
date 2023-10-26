400ab8da9e47393e263044b4194988f5af91725a
Taisei Fujino <raisu05150515@gmail.com>
2023-10-26 12:26:59 +0900

    a

--- test.bash
+++ test.bash
@@ -13,8 +13,12 @@ out=$(seq 5 | ./plus)
 [ "${out}" =15 ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]	 || ng ${LINENO}
-[ "${out}"= "" ] || ng ${LINENO}
+[ "$?" = 1 ]	  || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" =１ ]	  || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 
 [ "$res" =0 ] && echo OK
