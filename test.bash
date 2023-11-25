d158ca0da86bd984a77cadcf68640aa50cdce5c3
Taisei Fujino <raisu05150515@gmail.com>
2023-11-26 00:26:29 +0900

    Add

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Taisei Fujino
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -14,5 +14,14 @@ out=$(seq 5 | ./plus)
 
 [ "${out}" = 15 ] || ng ${LINENO}
 
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+
 [ "$res" = 0 ] && echo OK
-exit $res
+  exit $res
