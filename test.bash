d65007184db0b2daaf776ee112494257906ba9af
Taisei Fujino <raisu05150515@gmail.com>
2023-11-28 15:42:24 +0900

    Add

--- test.bash
+++ test.bash
@@ -6,22 +6,22 @@ ng() {
      echo NG at Line $1
      res=1
 
-}
+}     
 
 res=0
 
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ] || ng ${LINENO}
+  [ "${out}" = 15 ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
 
 
-[ "$res" = 0 ] && echo OK
+  [ "$res" = 0 ] && echo OK
   exit $res
